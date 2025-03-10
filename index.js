6de13ba216745c5f8f8213a535cbdddfcea70bca
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2025-03-10 11:28:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>hiii</h1>,document.getElementById("root"));
+ReactDOM.render(<h1>hiii, hello</h1>,document.getElementById("root"));
